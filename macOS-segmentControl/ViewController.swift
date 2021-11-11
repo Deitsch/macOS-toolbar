960fc5b9e03b2384e9efca07253c7ca4bfcae4da
Simon Deutsch <simon.deutsch97@gmail.com>
2021-11-11 10:47:46 +0100

    Initial Commit

---
+++ macOS-segmentControl/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  macOS-segmentControl
+//
+//  Created by Simon Deutsch on 11.11.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
